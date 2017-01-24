cfc94dc8af42509de41b3f5ffba14f6e39a7511b
Adrian Caramaliu <adrian@caramaliu.com>
2017-01-24 16:29:33 -0500

    v0.0.011.20170123 - ALPHA - Fixed a bug where account id was not hashed

--- smartapps/ady624/webcore-piston.src/webcore-piston.groovy
+++ smartapps/ady624/webcore-piston.src/webcore-piston.groovy
@@ -14,8 +14,9 @@
  *
 */
 
-def version() {	return "v0.0.010.20170123" }
+def version() {	return "v0.0.011.20170123" }
 /*
+ *	01/23/2016 >>> v0.0.011.20170123 - ALPHA - Fixed a bug where account id was not hashed
  *	01/23/2016 >>> v0.0.010.20170123 - ALPHA - Duplicate piston and restore from automatic backup :)
  *	01/23/2016 >>> v0.0.00f.20170123 - ALPHA - Automatic backup to myjson.com is now enabled. Restore is not implemented yet.
  *	01/22/2016 >>> v0.0.00e.20170122 - ALPHA - Enabled device cache on main app to speed up dashboard when using large number of devices
