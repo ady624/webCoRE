0c4f3cbf4c7681aaef133764b4a56583315eea81
Adrian Caramaliu <ady624>
2017-04-11 19:09:00 -0700

    v0.0.065.20170411 - ALPHA - Fix for long waits being converted to scientific notation, causing the scheduler to misunderstand them and wait 1ms instead

---
+++ dashboard/js/modules/register.module.js
@@ -0,0 +1,50 @@
+config.controller('register', ['$scope', '$rootScope', 'dataService', '$timeout', '$interval', '$location', '$sce', '$routeParams', 'ngDialog', '$window', function($scope, $rootScope, dataService, $timeout, $interval, $location, $sce, $routeParams, ngDialog, $window) {
+	var tmrStatus = null;
+	$scope.loading = false;
+	$scope.code = '';
+
+	$scope.init = function() {
+	};
+
+    $scope.setStatus = function(status) {
+        if (tmrStatus) $timeout.cancel(tmrStatus);
+        tmrStatus = null;
+        $scope.status = status;
+        if ($scope.status) {
+            tmrStatus = $timeout(function() { $scope.setStatus(); }, 10000);
+        }
+    }
+
+    $scope.$on('$destroy', function() {
+		if (tmrStatus) $timeout.cancel(tmrStatus);
+    });
+
+
+	$scope.register = function() {
+		$scope.loading = true;
+		dataService.registerDashboard($scope.code).then(function(data) {
+			if (data && (data.length >= 80) && (data.length <= 128)) {
+				$location.path('/init/' + data);
+			} else {
+				$scope.setStatus("Sorry, the registration code you provided did not work...");
+			}
+			$scope.loading = false;
+		});
+    };
+
+
+
+
+
+    //init
+	$scope.init();
+	var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+	if( userAgent.match( /Android/i ) ) {
+		$scope.android = true;
+	}
+	$scope.url = window.location.href;
+	$scope.mobile = window.mobileCheck();
+	$scope.tablet = (!$scope.mobile) && (window.mobileOrTabletCheck());
+	$scope.formatTime = formatTime;
+    $scope.utcToString = utcToString;
+}]);
