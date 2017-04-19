2dd5321a3c9bb38bb1032491831aef5b71278e29
Adrian Caramaliu <ady624>
2017-04-19 09:49:57 -0700

    v0.0.07a.20170419 - ALPHA - Minor bug fixes, triggers inside timers no longer subscribe to events (the timer is a trigger itself) - triggers should not normally be used inside timers

--- dashboard/js/app.js
+++ dashboard/js/app.js
@@ -1069,4 +1069,4 @@ if (document.selection) {
 }}
 
 //navigator.registerProtocolHandler('web+core','https://' + window.location.hostname + '/handler/%s', 'webCoRE');
-version = function() { return 'v0.0.079.20170419'; };
+version = function() { return 'v0.0.07a.20170419'; };
