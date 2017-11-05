4db912aa546a1d99a8e6166d92530aa264ce5c19
Adrian Caramaliu <adrian@caramaliu.com>
2017-11-05 08:45:43 -0500

    v0.2.0fc.20171105 - BETA M2 - DST fixes

--- smartapps/ady624/webcore-dashboard.src/webcore-dashboard.groovy
+++ smartapps/ady624/webcore-dashboard.src/webcore-dashboard.groovy
@@ -16,7 +16,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-public static String version() { return "v0.2.0fb.20171026" }
+public static String version() { return "v0.2.0fc.20171105" }
 /******************************************************************************/
 /*** webCoRE DEFINITION														***/
 /******************************************************************************/
